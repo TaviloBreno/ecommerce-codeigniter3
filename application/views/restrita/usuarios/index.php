a73f05cd869fdcf7bd52b835c460cb7fab2241b8
Tavilo Breno <breno_wk2@hotmail.com>
2024-06-11 01:50:33 -0300

    Listando os usuários do banco

---
+++ application/views/restrita/usuarios/index.php
@@ -0,0 +1,19 @@
+		<?php $this->load->view('restrita/layout/navbar'); ?>
+		<?php $this->load->view('restrita/layout/sidebar'); ?>
+		<!-- Main Content -->
+		<div class="main-content">
+			<section class="section">
+				<div class="section-body">
+					<!-- add content here -->
+
+
+
+				</div>
+			</section>
+			<div class="settingSidebar">
+				<a href="javascript:void(0)" class="settingPanelToggle"> <i class="fa fa-spin fa-cog"></i>
+				</a>
+				<?php $this->load->view('restrita/layout/sidebar-settings'); ?>
+			</div>
+		</div>
+
