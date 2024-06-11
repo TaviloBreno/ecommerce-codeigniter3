f2531f754cf543d0041607699c8d0a91a73b2f4b
Tavilo Breno <breno_wk2@hotmail.com>
2024-06-11 00:02:08 -0300

    Realizando o fatiamento do template da área restrita

--- application/controllers/restrita/Home.php
+++ application/controllers/restrita/Home.php
@@ -10,8 +10,8 @@ class Home extends CI_Controller
 
 	public function index()
 	{
-		//$this->load->view('restrita/layout/header');
+		$this->load->view('restrita/layout/header');
 		$this->load->view('restrita/home/index');
-		//$this->load->view('restrita/layout/footer');
+		$this->load->view('restrita/layout/footer');
 	}
 }
